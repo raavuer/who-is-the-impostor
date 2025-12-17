e93c2398e03ac92107fb73098ce6ce740f699dc2
Micah <micahronan@proton.me>
2025-12-16 22:02:02 -0600

    Refactored most the code cause I hated it

--- index.js
+++ index.js
@@ -10,54 +10,26 @@ const io = new Server(server);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.get("/", (req, res) => {res.sendFile(join(__dirname, 'index.html'));});
 
-const settings = {
-  numberOfPlayers: 4,
-  numberOfImpostors: 1,
-};
-const players = {};
-function Player(name) {
-  this.name = name;
-  this.role = "Unassigned";
-  this.vote = "No one";
-}
-
-function startGame() {
-  for (let i = 0; i < settings.numberOfPlayers; i++) {
-    players[Object.keys(players)[i]].role = "Bystander";
-  }
-  for (let i = 0; i < settings.numberOfImpostors; i++) {
-    // FIX - Allows one person to be impostor multiple times
-    players[Object.keys(players)[Math.floor(Math.random() * settings.numberOfPlayers)]].role = "Impostor";
-  }
-  for (const player of Object.keys(players)) {
-    io.to(player).emit("getRole", players[player]);
-  }
-}
+const players = [];
 
 io.on("connection", (socket) => {
-  players[socket.id] = new Player(socket.id);
   socket.on("disconnect", () => {
-    delete players[socket.id];
-    io.emit("getPlayersNames", players); // Probably gives client too much trust (can see roles possibly)
+    players.splice(players.findIndex((player) => {player.id === socket.id}), 1);
+    console.log(players);
+    io.emit("changePlayers", players);
   });
-  socket.on("changeName", (name) => {
-    players[socket.id].name = name.match(/\w{3,10}/);
-    io.emit("getPlayersNames", players); // Probably gives client too much trust (can see roles possibly)
-  });
-  if (Object.keys(players).length < settings.numberOfPlayers) {
-    return;
-  }
-  startGame();
-  socket.once("voteFor", (votedFor) => {
-    players[socket.id].vote = votedFor;
-    console.log(`${players[socket.id].name} voted for ${players[socket.id].vote}`);
+  io.to(socket.id).timeout(10000).emit("getPlayerName", (error, playerName) => {
+    if (error) {
+      console.log(`Couldn't get player name: ${error}`);
+    } else {
+      players.push({
+        id: socket.id,
+        name: playerName.toString().match(/\w{3,10}/).toString()
+      });
+      console.log(players);
+      io.emit("changePlayers", players);
+    }
   });
 });
 
 server.listen(8080, () => {console.log('App started.');});
-
-/* TODO
- * Remove cheating through getPlayersNames and browser network
- * Add voting confirmation for client
- *
-*/
